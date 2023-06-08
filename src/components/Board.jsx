91c37cdcb1558ecb54d79a37d078cf12d8eff67e
Aryaman <201b064@juetguna.in>
2023-06-08 17:21:38 +0530

    Added Winner History and Status message functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,62 +1,39 @@
-import { useState } from "react";
 import Square from "./Square";
-const Board = () =>{
-    const [squares,setSquares]=useState(Array(9).fill(null));
-
-    const [isXNext,setIsXNesxt] = useState(false);
-
-    const handleSquareClick = clickedPosition =>{   
-
-        if(squares[clickedPosition]){
-            return;
-        }
-
-        setSquares((currentSquares) =>{
-            
-            return currentSquares.map((sqaureValue ,position ) =>{
-                
-                if(clickedPosition ===position){
-                    return isXNext ? 'X' : 'O';
-                }
-                return sqaureValue;
-            
-            })
-        });
 
 
-        setIsXNesxt((currentIsXNext) => !currentIsXNext)
-    }
 
+const Board = ({squares, handleSquareClick}) =>{
+    
     const renderSquare =(position) =>{
-        return <Square value={squares[position]} onClick = {()=>{
-            handleSquareClick(position);
-        }}/>
-    }
+        return <Square value={squares[position]} onClick = {()=>
+            handleSquareClick(position)}/>
+    };
 
 
 
 
     return (
         <div className="board">
-        <div className="board-row">
+            <div className="board-row">
             {renderSquare(0)}
             {renderSquare(1)}
             {renderSquare(2)}
 
-        </div>
-        <div className="board-row">
+            </div>
+            <div className="board-row">
             {renderSquare(3)}
             {renderSquare(4)}
             {renderSquare(5)}
 
-        </div>
-        <div className="board-row">
+            </div>
+            <div className="board-row">
             {renderSquare(6)}
             {renderSquare(7)}
             {renderSquare(8)}
-        </div>
+            </div>
     </div>
     );
-    };
+
+};
 
 export default Board;
